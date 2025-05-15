bbe1d7bc8a9d73680faa8ae0910444eee9c2daa9
fahmida-faiza <fahmidafaiza57@gmail.com>
2025-05-15 22:08:49 +0600

    added cancel

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -2,30 +2,63 @@ import { useForm } from 'react-hook-form';
 import Modal from '../ui/Modal'
 
 const AddTaskModal = ({isOpen, setIsOpen }) => {
-    const {register, handleSubmit} = useForm()
+    const {register, handleSubmit, reset} = useForm()
     const onSubmit = (data) => {
         console.log(data);
+        onCancel();
     }
+
+// cancel button
+const onCancel = () =>{
+    reset();
+    setIsOpen(false)
+}
+
+
     return (
         <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="programming here"
         >
            
             <form onSubmit={handleSubmit(onSubmit)}>
 
-        <div className='flex flex-col gap-3'>
+        <div className='flex flex-col gap-3 my-4'>
             <label htmlFor="title">Title</label>
-            <input type="text" {...register('name')} />
-            <input type="date" className="input" />
-            <label htmlFor="title">Title</label>
-            <input type="text" {...register('name')} />
+            <input type="text" id="title" {...register('title')} />
+                        <label htmlFor="title">Date</label>
+
+            <input type="date" id="date" {...register('date')} className="input" />
+
+                                    <label htmlFor="title">Description</label>
+                                    <textarea className="textarea" id="description" {...register('description')} placeholder="description"></textarea>
+
+
+
+
+
+  <label htmlFor="title">Priority</label>
+<select defaultValue="Priority" id="priority"{...register('priority')} className="select">
+  
+  <option>Crimson</option>
+  <option>Amber</option>
+  <option>Velvet</option>
+</select>
+
             
 
-        </div>
+</div>
+            
+
+      
+
+<div className='flex justify-end gap-3'>
+    <button onClick={() => onCancel()} type='button' className='btn btn-danger'>
+        Cancel
+
+    </button>
 
 
-
-        <button type="submit">Submit</button>
-
+        <button type="submit" className='btn btn-primary'>Submit</button>
+</div>
             </form>
       </Modal>
     );
