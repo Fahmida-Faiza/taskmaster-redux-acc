9d28c54a023736742e9a2ffc68aee6ff7d3f2ec9
fahmida-faiza <fahmidafaiza57@gmail.com>
2025-05-22 22:09:52 +0600

    added my tas konly user mir hussain

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -3,9 +3,9 @@ import Modal from '../ui/Modal'
 import { useDispatch } from 'react-redux';
 import { addTask } from '../../redux/features/tasks/tasksSlice';
 
-const AddTaskModal = ({isOpen, setIsOpen }) => {
-    const {register, handleSubmit, reset} = useForm()
-const dispatch = useDispatch()
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+    const { register, handleSubmit, reset } = useForm()
+    const dispatch = useDispatch()
 
 
 
@@ -15,72 +15,159 @@ const dispatch = useDispatch()
         dispatch(addTask(data))
         onCancel();
     }
- 
-// cancel button
-const onCancel = () =>{
-    reset();
-    setIsOpen(false)
-}
+
+    // cancel button
+    const onCancel = () => {
+        reset();
+        setIsOpen(false)
+    }
 
 
     return (
         <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="programming here"
         >
-           
+
             <form onSubmit={handleSubmit(onSubmit)}>
 
-        <div className='flex flex-col gap-3 my-4'>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" {...register('title')} />
-                        <label htmlFor="title">Date</label>
+                <div className='flex flex-col gap-3 my-4'>
+                    <label htmlFor="title">Title</label>
+                    <input type="text" id="title" {...register('title')} />
+                    <label htmlFor="title">Date</label>
 
-            <input type="date" id="date" {...register('date')} className="input" />
+                    <input type="date" id="date" {...register('date')} className="input" />
 
-                                    <label htmlFor="title">Description</label>
-                                    <textarea className="textarea" id="description" {...register('description')} placeholder="description"></textarea>
-
-
-
-<label htmlFor="title">
-    Assign to
-</label>
-<select 
-id="assignedTo"
-{...register('assignedTo')} className="select"
->
-    <option value="Mir hossain">Mir hossain</option>
-    <option value="Nahid hossain">Nahid hossain</option>
-    <option value="Samin">Samin</option>
-    <option value="Ratul">Ratul</option>
-    </select>
-
-  <label htmlFor="title">Priority</label>
-<select defaultValue="Priority" id="priority"{...register('priority')} className="select">
-  
-  <option>high</option>
-  <option>medium</option>
-  <option>low</option>
-</select>
-
-            
-
-</div>
-            
-
-      
-
-<div className='flex justify-end gap-3'>
-    <button onClick={() => onCancel()} type='button' className='btn btn-danger'>
-        Cancel
-
-    </button>
+                    <label htmlFor="title">Description</label>
+                    <textarea className="textarea" id="description" {...register('description')} placeholder="description"></textarea>
 
 
-        <button type="submit" className='btn btn-primary'>Submit</button>
-</div>
+
+                    <label htmlFor="title">
+                        Assign to
+                    </label>
+                    <select
+                        id="assignedTo"
+                        {...register('assignedTo')} className="select"
+                    >
+                        <option value="Mir Hussain"> Mir Hussain</option>
+                        <option value="Nahid hossain">Nahid hossain</option>
+                        <option value="Samin">Samin</option>
+                        <option value="Ratul">Ratul</option>
+                    </select>
+
+                    <label htmlFor="title">Priority</label>
+                    <select defaultValue="Priority" id="priority"{...register('priority')} className="select">
+
+                        <option>high</option>
+                        <option>medium</option>
+                        <option>low</option>
+                    </select>
+
+
+
+                </div>
+
+
+
+
+                <div className='flex justify-end gap-3'>
+                    <button onClick={() => onCancel()} type='button' className='btn btn-danger'>
+                        Cancel
+
+                    </button>
+
+
+                    <button type="submit" className='btn btn-primary'>Submit</button>
+                </div>
             </form>
-      </Modal>
+        </Modal>
     );
 };
 
 export default AddTaskModal;
+
+
+
+// ai help
+
+// import { useForm } from 'react-hook-form';
+// import Modal from '../ui/Modal';
+// import { useDispatch } from 'react-redux';
+// import { addTask } from '../../redux/features/tasks/tasksSlice';
+
+// const AddTaskModal = ({ isOpen, setIsOpen }) => {
+//   const { register, handleSubmit, reset } = useForm();
+//   const dispatch = useDispatch();
+
+//   const onSubmit = (data) => {
+//     dispatch(addTask(data)); // keep assignedTo from form
+//     onCancel();
+//   };
+
+//   const onCancel = () => {
+//     reset();
+//     setIsOpen(false);
+//   };
+
+//   return (
+//     <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="programming here">
+//       <form onSubmit={handleSubmit(onSubmit)}>
+//         <div className='flex flex-col gap-3 my-4'>
+//           <label htmlFor="title">Title</label>
+//           <input type="text" id="title" {...register('title')} />
+
+//           <label htmlFor="date">Date</label>
+//           <input type="date" id="date" {...register('date')} className="input" />
+
+//           <label htmlFor="description">Description</label>
+//           <textarea
+//             className="textarea"
+//             id="description"
+//             {...register('description')}
+//             placeholder="description"
+//           ></textarea>
+
+//           <label htmlFor="assignedTo">Assign to</label>
+//           <select
+//             id="assignedTo"
+//             {...register('assignedTo')}
+//             className="select"
+//           >
+//             {/* ✅ Make sure these match exactly with userSlice.name values */}
+//             <option value="Mir Hussain">Mir Hussain</option>
+//             <option value="Nahid Hossain">Nahid Hossain</option>
+//             <option value="Samin">Samin</option>
+//             <option value="Ratul">Ratul</option>
+//           </select>
+
+//           <label htmlFor="priority">Priority</label>
+//           <select
+//             defaultValue="Priority"
+//             id="priority"
+//             {...register('priority')}
+//             className="select"
+//           >
+//             <option value="high">high</option>
+//             <option value="medium">medium</option>
+//             <option value="low">low</option>
+//           </select>
+//         </div>
+
+//         <div className='flex justify-end gap-3'>
+//           <button
+//             onClick={onCancel}
+//             type='button'
+//             className='btn btn-danger'
+//           >
+//             Cancel
+//           </button>
+
+//           <button type="submit" className='btn btn-primary'>
+//             Submit
+//           </button>
+//         </div>
+//       </form>
+//     </Modal>
+//   );
+// };
+
+// export default AddTaskModal;
--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -5,42 +5,45 @@ import {
 import { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { userTasks } from '../../redux/features/tasks/tasksSlice';
-
+import {userTasks} from "../../redux/features/tasks/tasksSlice"
 const MyTasks = () => {
-const { tasks} = useSelector((state) => state.tasksSlice)
-
-const {name : userName} = useSelector((state) => state.userSlice)
-// console.log(name)
+  const { tasks, userSpecificTasks } = useSelector((state) => state.tasksSlice)
+ 
 
 
-const dispatch = useDispatch()
-useEffect(() => {
-  dispatch(userTasks(userName))
-}, [userName,dispatch]);
+  const { name: userName } = useSelector((state) => state.userSlice)
+  // console.log( userSpecificTasks)
+
+
+  const dispatch = useDispatch()
+
+
+
+  useEffect(() => {
+    dispatch(userTasks(userName))
+  }, [userName, dispatch ,tasks ]);
 
   return (
     <div>
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
-       {tasks?. map((item) => (
+        {userSpecificTasks?.map((item) => (
 
-
- <div
-          key={item.id}
-          className="bg-secondary/10 rounded-md p-3 flex justify-between"
-        >
-          <h1>{item.title}</h1>
-          <div className="flex gap-3">
-            <button className="grid place-content-center" title="Details">
-              <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
-            </button>
-            <button className="grid place-content-center" title="Done">
-              <CheckIcon className="w-5 h-5 text-primary" />
-            </button>
+          <div
+            key={item.id}
+            className="bg-secondary/10 rounded-md p-3 flex justify-between"
+          >
+            <h1>{item.title}</h1>
+            <div className="flex gap-3">
+              <button className="grid place-content-center" title="Details">
+                <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
+              </button>
+              <button className="grid place-content-center" title="Done">
+                <CheckIcon className="w-5 h-5 text-primary" />
+              </button>
+            </div>
           </div>
-        </div>
-       ))}
+        ))}
       </div>
     </div>
   );
